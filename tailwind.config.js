aeb3652602688f736a470968d610567be3c08b92
Michael Kopchenov <121085772+MichaelKopchenov@users.noreply.github.com>
2024-12-02 14:15:39 +0300

    changed padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,27 +6,26 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
   // Указание файлов, в которых будут сканироваться классы TailwindCSS
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",      // Все файлы в папке pages
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // Все файлы в папке pages
     "./components/**/*.{js,ts,jsx,tsx,mdx}", // Все файлы в папке components
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",        // Все файлы в папке app
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Все файлы в папке app
   ],
 
   theme: {
     // Настройка контейнера (центрирование и отступы)
     container: {
-      center: true,    // Центрирует контейнер
-      padding: "8px", // Добавляет внутренний отступ
+      center: true, // Центрирует контейнер
+      padding: "16px", // Добавляет внутренний отступ
     },
 
     // Определение экранных размеров (breakpoints)
     screens: {
-      sm: "640px",  // @media (min-width: 640px)
-      md: "768px",  // @media (min-width: 768px)
+      sm: "640px", // @media (min-width: 640px)
+      md: "768px", // @media (min-width: 768px)
       lg: "1024px", // @media (min-width: 1024px)
-      xl: "1280px"  // @media (min-width: 1280px)
+      xl: "1280px", // @media (min-width: 1280px)
     },
 
     // Определение шрифтов
@@ -37,10 +36,10 @@ module.exports = {
     extend: {
       // Добавление собственных цветов
       colors: {
-        primary: "#1c1c22",   // Основной цвет
+        primary: "#1c1c22", // Основной цвет
         accent: {
           DEFAULT: "#0081a7", // Акцентный цвет по умолчанию
-          hover: "#00e187",   // Цвет при наведении
+          hover: "#00e187", // Цвет при наведении
         },
       },
     },
