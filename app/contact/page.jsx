e0b5c0615c0d7d9cc13162cc8e95a440a471a9f8
Michael Kopchenov <121085772+MichaelKopchenov@users.noreply.github.com>
2024-12-03 09:28:52 +0300

    changed padding

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,24 @@
 export default function Contact() {
-    return (
-        <div>Контакты</div>
-    );
+  return (
+    <div className="flex flex-column place-content-evenly">
+      <ul>
+        <h2>Контактная информация</h2>
+        <li>fdd-info@yandex.ru</li>
+        <li>+7 (499) 322-76-60</li>
+        <li>Москва, пр-т Вернадского 82</li>
+      </ul>
+      <ul>
+        <h2>Основные ссылки</h2>
+        <li>Главная</li>
+        <li>Наши проекты</li>
+        <li>Наши услуги</li>
+      </ul>
+      <ul>
+        <h2>Социальные сети</h2>
+        <li>Telegram</li>
+        <li>VK</li>
+        <li>GitHub</li>
+      </ul>
+    </div>
+  );
 }
