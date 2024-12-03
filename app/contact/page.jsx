fbcdc1bbf231c60298f10df855e9b88f509bc156
Рябов Сергей Дмитриевич <ryabov-sd@ranepa.ru>
2024-12-03 14:48:22 +0300

    page contact

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,63 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue
+} from "@/components/ui/select";
+
+
+
+
 export default function Contact() {
     return (
-        <div>Контакты</div>
+        <section className="py-6">
+            <div className="container mx-auto">
+                <div className="flex flex-col xl:flex-row gap-[30px]">
+                    {/* form */}
+                    <div className="xl:h-[54%] order-2 xl:order-none">
+                        <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+                            <h3 className="text-4xl text-accent">
+                                Погнали брата! Пиши своё сообщение!
+                            </h3>
+                            <p>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit architecto quo esse libero accusamus sed at! Illo culpa aspernatur beatae, debitis, velit ipsam harum ad provident necessitatibus, quos dignissimos exercitationem.
+                            </p>
+                            {/* input */}
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                                <Input type="firstname" placeholder="Имя" />
+                                <Input type="lastname" placeholder="Фамилия" />
+                                <Input type="email" placeholder="Почта" />
+                                <Input type="phone" placeholder="Номер телефона" />
+                            </div>
+                            {/* select */}
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Выберите услугу" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Выберите услугу</SelectLabel>
+                                        <SelectItem value="est">Веб-разработка</SelectItem>
+                                        <SelectItem value="cts">Веб-разработка</SelectItem>
+                                        <SelectItem value="mst">Веб-разработка</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+                        </form>
+                    </div>
+                    {/* info */}
+                    <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">info</div>
+                </div>
+            </div>
+        </section>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // Указание файлов, в которых будут сканироваться классы TailwindCSS
+    darkMode: ["class"],
+    // Указание файлов, в которых будут сканироваться классы TailwindCSS
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}", // Все файлы в папке pages
     "./components/**/*.{js,ts,jsx,tsx,mdx}", // Все файлы в папке components
@@ -14,36 +15,34 @@ module.exports = {
   ],
 
   theme: {
-    // Настройка контейнера (центрирование и отступы)
-    container: {
-      center: true, // Центрирует контейнер
-      padding: "16px", // Добавляет внутренний отступ
-    },
-
-    // Определение экранных размеров (breakpoints)
-    screens: {
-      sm: "640px", // @media (min-width: 640px)
-      md: "768px", // @media (min-width: 768px)
-      lg: "1024px", // @media (min-width: 1024px)
-      xl: "1280px", // @media (min-width: 1280px)
-    },
-
-    // Определение шрифтов
-    fontFamily: {
-      primary: "var(--font-jetbrainsMono)", // Используем шрифт из globals.css
-    },
-
-    extend: {
-      // Добавление собственных цветов
-      colors: {
-        primary: "#1c1c22", // Основной цвет
-        accent: {
-          DEFAULT: "#0081a7", // Акцентный цвет по умолчанию
-          hover: "#00e187", // Цвет при наведении
-        },
-      },
-    },
+  	container: {
+  		center: 'true',
+  		padding: '16px'
+  	},
+  	screens: {
+  		sm: '640px',
+  		md: '768px',
+  		lg: '1024px',
+  		xl: '1280px'
+  	},
+  	fontFamily: {
+  		primary: 'var(--font-jetbrainsMono)'
+  	},
+  	extend: {
+  		colors: {
+  			primary: '#1c1c22',
+  			accent: {
+  				DEFAULT: '#0081a7',
+  				hover: '#00e187'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   // Список подключаемых плагинов (по мере необходимости)
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
